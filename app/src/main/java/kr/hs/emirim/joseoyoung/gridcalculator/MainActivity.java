0bd77b9ec7f97e5129ead867824e9435ccda62d2
joseoyoung <josy3465@naver.com>
2017-04-11 19:50:12 +0900

    #1 GridCalculator

--- app/src/main/java/kr/hs/emirim/joseoyoung/gridcalculator/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/joseoyoung/gridcalculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit1, edit2;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     public static final int SELECT_EDIT2=1;
     int selectEdit=SELECT_EDIT1;
     String numStr="";
+    TextView textResult;
 //    int[] ids={R.id.but_0,R.id.but_1,R.id.but_2,R.id.but_3, R.id.but_4,
 //            R.id.but_5,R.id.but_6,R.id.but_7,R.id.but_8,R.id.but_9};
     @Override
@@ -23,8 +25,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         edit1 = (EditText)findViewById(R.id.edit1);
         edit2 = (EditText)findViewById(R.id.edit2);
+        edit1.setText("0");
+        edit2.setText("0");
         edit1.setOnTouchListener(editHandler);
         edit2.setOnTouchListener(editHandler);
+        textResult=(TextView)findViewById(R.id.text_result);
 
         for(int i=0;i<butNums.length;i++){
             butNums[i]=(Button)findViewById(R.id.but0+i);
@@ -55,7 +60,24 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener butOpHandler=new View.OnClickListener(){
         @Override
         public void onClick(View view) {
-
+            int num1=Integer.parseInt(edit1.getText().toString());
+            int num2=Integer.parseInt(edit2.getText().toString());
+            double result=0;
+            switch (view.getId()) {
+                case R.id.but_op1:
+                    result=num1+num2;
+                    break;
+                case R.id.but_op2:
+                    result=num1-num2;
+                    break;
+                case R.id.but_op3:
+                    result=num1*num2;
+                    break;
+                case R.id.but_op4:
+                    result=(double)num1/num2;
+                    break;
+            }
+            textResult.setText("* 계산결과 = "+result);
         }
     };
 
